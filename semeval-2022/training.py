6491f057221e09d1cee72c1eaf5469a9ad990dfb
mc821@ic.ac.uk <mc821@ic.ac.uk>
2022-02-23 15:05:58 +0000

    made some changes to code but still need to check

--- semeval-2022/training.py
+++ semeval-2022/training.py
@@ -7,14 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1MD7qB_hbVgR43nbACRhL4wfqqEv5pJj3
 """
 
-# from google.colab import drive
-# drive.mount('/content/drive')
-
-# Commented out IPython magic to ensure Python compatibility.
-# %cd drive/MyDrive/NLPCW
-# %pwd
-# pip install transformers
-
 import pandas as pd
 import transformers
 from transformers import BertModel, BertTokenizer, RobertaConfig, RobertaModel
@@ -51,32 +43,16 @@ tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 sample_txt = 'Hello! I love that you are so poor.'
 tokens_sample = tokenizer.tokenize(sample_txt)
 token_ids = tokenizer.convert_tokens_to_ids(tokens_sample)
-print(token_ids) 
-print(len(token_ids)) 
+print(token_ids)
+print(len(token_ids))
 print(tokens_sample)
 
 print(tokenizer.sep_token, tokenizer.sep_token_id) # end of sentence marker
-print(tokenizer.cls_token, tokenizer.cls_token_id) # start of sentence token 
+print(tokenizer.cls_token, tokenizer.cls_token_id) # start of sentence token
 print(tokenizer.pad_token, tokenizer.pad_token_id) # token for padding
 print(tokenizer.unk_token, tokenizer.unk_token_id) # token for unknown
 
 """Then, we do embedding on the tokens. """
-
-# encoding = tokenizer.encode_plus(
-#   sample_txt,
-#   max_length=32,
-#   truncation = True, # truncate examples to max length 
-#   add_special_tokens=True, # Add '[CLS]' and '[SEP]'
-#   return_token_type_ids=False,
-#   padding = "max_length", 
-#   return_attention_mask=True,
-#   return_tensors='pt',  # Return PyTorch tensors
-# )
-
-# print(encoding.keys()) # dict_keys(['input_ids', 'attention_mask'])
-# print(encoding.input_ids)
-
-# print(encoding['attention_mask'])
 
 class SentimentClassifier(nn.Module):
   def __init__(self, n_classes):
@@ -94,13 +70,13 @@ class SentimentClassifier(nn.Module):
 
 df_train, df_test = train_test_split(
   df,
-  test_size=0.3, 
+  test_size=0.3,
   random_state = RANDOM_SEED
 )
 
 df_test, df_val = train_test_split(
   df_test,
-  test_size=0.5, 
+  test_size=0.5,
   random_state = RANDOM_SEED
 )
 
@@ -110,32 +86,28 @@ df_train.shape, df_val.shape, df_test.shape
 
 class MyDataset(Dataset):
 
-  def __init__(self,dataframe, tokenizer, max_len):
+  def __init__(self,dataframe, tokenizer):
     self.x=dataframe.loc[:,['text']]
     self.y=dataframe.loc[:,['label']]
 
     self.x = np.array(self.x)
     self.y = np.array(self.y)
-    
-    # self.x_train=torch.from_numpy(self.x)
-    # self.y_train=torch.from_numpy(self.y)
+
     self.tokenizer = tokenizer
-    self.max_len = max_len
 
   def __len__(self):
     return len(self.y)
-  
+
   def __getitem__(self,idx):
     input = str(self.x[idx])
     labels = self.y[idx]
 
     encoding = tokenizer.encode_plus(
       input,
-      max_length= self.max_len,
-      truncation = True, # truncate examples to max length 
+      truncation = True, # truncate examples to max length
       add_special_tokens=True, # Add '[CLS]' and '[SEP]'
       return_token_type_ids=False,
-      padding = "max_length", 
+      padding = "max_length",
       return_attention_mask=True,
       return_tensors='pt')  # Return PyTorch tensor
 
@@ -146,11 +118,10 @@ class MyDataset(Dataset):
       'targets': torch.tensor(labels, dtype=torch.long)
     }
 
-def create_data_loader(df, tokenizer, max_len, batch_size):
+def create_data_loader(df, tokenizer, batch_size):
   ds = MyDataset(
     dataframe = df,
     tokenizer=tokenizer,
-    max_len=max_len
   )
 
   return DataLoader(
@@ -160,66 +131,48 @@ def create_data_loader(df, tokenizer, max_len, batch_size):
   )
 
 BATCH_SIZE = 32
-MAX_LEN = 160 # can change this 
-train_data_loader = create_data_loader(df_train, tokenizer, MAX_LEN, BATCH_SIZE)
-test_data_loader = create_data_loader(df_test, tokenizer, MAX_LEN, BATCH_SIZE)
-val_data_loader = create_data_loader(df_val, tokenizer, MAX_LEN, BATCH_SIZE)
+train_data_loader = create_data_loader(df_train, tokenizer, BATCH_SIZE)
+test_data_loader = create_data_loader(df_test, tokenizer, BATCH_SIZE)
+val_data_loader = create_data_loader(df_val, tokenizer, BATCH_SIZE)
 
 """# Classification"""
 
+encoding = tokenizer.encode_plus(
+      sample_txt,
+      max_length = 500,
+      truncation = True, # truncate examples to max length
+      add_special_tokens=True, # Add '[CLS]' and '[SEP]'
+      return_token_type_ids=False,
+      padding = "max_length",
+      return_attention_mask=True,
+      return_tensors='pt')  # Return PyTorch tensor
+
+# print(encoding["input_ids"].shape)
+# print(encoding['input_ids'])
+# print(encoding['attention_mask'])
+
 from transformers import RobertaModel, RobertaConfig
 
-configuration = RobertaConfig()
-model = RobertaModel(configuration)
-output = model(
-  input_ids=encoding['input_ids'],
-  attention_mask=encoding['attention_mask']
-)
-
-print (output[1].shape)
-
-# cuda_available = torch.cuda.is_available()
-# print(cuda_available)
-
-# if cuda_available:
-#   import tensorflow as tf
-#   # Get the GPU device name.
-#   device = tf.test.gpu_device_name()
-#   # The device name should look like the following:
-#   if device == '/device:GPU:0':
-#       print('Found GPU at: {}'.format(device))
-#   else:
-#       raise SystemError('GPU device not found')
-
-# else:
-#     device = "cpu"
-
-# print(device)
-
 GPU = True
-if GPU: 
+if GPU:
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 else:
   device = torch.device("cpu")
 print(f"Using {device}")
 
-
 class SentimentClassifier(nn.Module):
   def __init__(self, n_classes):
     super(SentimentClassifier, self).__init__()
-    # self.bert = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
-    self.model = model
+    configuration = RobertaConfig()
+    self.model = RobertaModel(configuration)
     self.drop = nn.Dropout(p=0.3)
     self.out = nn.Linear(768, n_classes)
-    
+
   def forward(self, input_ids, attention_mask):
     output = self.model(
       input_ids=input_ids,
       attention_mask=attention_mask
     )
-    
-    # print(output[1])
-    # print(output[1].shape)
 
     output = self.drop(output[1])
     return self.out(output)
@@ -265,26 +218,29 @@ def train_epoch(
   return correct_predictions.double() / n_examples, np.mean(losses)
 
 EPOCHS = 1
-device = 'cpu'
+# device = 'cpu'
+
+model = SentimentClassifier(n_classes=2)
+print(model)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
 total_steps = len(train_data_loader) * EPOCHS
 
 loss_fn = nn.CrossEntropyLoss().to(device)
 
-for epoch in range(EPOCHS):
+# for epoch in range(EPOCHS):
 
-  print(f'Epoch {epoch + 1}/{EPOCHS}')
-  print('-' * 10)
+#   print(f'Epoch {epoch + 1}/{EPOCHS}')
+#   print('-' * 10)
 
-  train_acc, train_loss = train_epoch(
-    SentimentClassifier(2),
-    train_data_loader,
-    loss_fn,
-    optimizer,
-    device,
-    n_examples = len(df_train)
-  )
+#   train_acc, train_loss = train_epoch(
+#     SentimentClassifier(2),
+#     train_data_loader,
+#     loss_fn,
+#     optimizer,
+#     device,
+#     n_examples = len(df_train)
+#   )
 
-print(f'Train loss {train_loss} accuracy {train_acc}')
+# print(f'Train loss {train_loss} accuracy {train_acc}')
 
