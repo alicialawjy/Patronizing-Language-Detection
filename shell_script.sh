729ff19878cff615f950749aba53c0a40ea16053
alicialawjy <alicialawjy@gmail.com>
2022-02-25 17:37:48 +0000

    shell script update

--- shell_script.sh
+++ shell_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --mail-type=ALL # required to send email notifcations
-#SBATCH --mail-user=ajl115 # required to send email notifcations - please replace <your_username> with your college login name or email address
 export PATH=/vol/bitbucket/ajl115/myvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
