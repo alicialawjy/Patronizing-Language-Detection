8e625798d6a98cde98d82fbf4d2febd5f1a0b6d4
weijiechua <weijiechua9512@gmail.com>
2022-02-28 21:16:09 +0000

    trying balanced

--- training_st.py
+++ training_st.py
@@ -68,7 +68,7 @@ class OlidDataset(Dataset):
 
 def hyperparam_tuning():
   optimizer = ['AdamW']
-  learning_rate = [2e-05, 4e-05, 6e-05]
+  learning_rate = [9e-04, 2e-05, 4e-05]
   epochs = [1,2,3]
   
   best_model = None
@@ -119,7 +119,8 @@ def hyperparam_tuning():
     pass
 
   try:
-    df = pd.DataFrame(param_dict)
+    df = pd.DataFrame.from_dict(param_dict)
+    print(df)
     df.to_csv("df_param.csv", orient='index')
   except:
     print(param_dict)
@@ -155,7 +156,7 @@ if __name__ == "__main__":
   # Read the data file
   
 
-  df_train = pd.read_csv('datasets/df_downsample.csv', index_col=0)
+  df_train = pd.read_csv('datasets/balanced_data/df_downsample.csv', index_col=0)
   df_val = pd.read_csv('datasets/df_val.csv', index_col=0)
   df_test = pd.read_csv('datasets/df_test.csv', index_col=0)
   trainset = reader(df_train)
