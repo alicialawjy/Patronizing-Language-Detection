9532f560260a57179d80e6cea8be34c3f8b142f4
alicialawjy <alicialawjy@gmail.com>
2022-02-28 11:10:34 +0000

    fix device

--- training_al2.py
+++ training_al2.py
@@ -110,7 +110,7 @@ def predict_condescending(input,tokenizer,model):
   encodings = tokenizer(input, return_tensors='pt', padding=True, truncation=True).to(device)
 
   output = model(**encodings).to(device)
-  preds = torch.max(output,1).to(device)
+  preds = torch.max(output,1)
 
   return {'prediction': preds[1], 'confidence': preds[0]}
 
@@ -124,8 +124,8 @@ def evaluate(model, tokenizer, data_loader):
 
   with torch.no_grad():
     for data in tqdm(data_loader): 
-      labels = data['label'].to(device)
-      text = data['text'].to(device)
+      labels = data['label']
+      text = data['text']
 
       pred = predict_condescending(text, tokenizer, model)
       preds.append(pred['prediction'].tolist())
