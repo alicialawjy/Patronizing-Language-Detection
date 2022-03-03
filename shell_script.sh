d7386722ef219854f81caa2940cc72f4b7193d03
alicialawjy <alicialawjy@gmail.com>
2022-03-03 00:06:05 +0000

    hugging face model

--- shell_script.sh
+++ shell_script.sh
@@ -4,6 +4,6 @@ export PATH=/vol/bitbucket/ajl115/myvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 TERM=vt100 # or TERM=xterm
-python3 synreplace-and-para.py
+python3 hugging-face-implementation.py
 /usr/bin/nvidia-smi
 uptime
