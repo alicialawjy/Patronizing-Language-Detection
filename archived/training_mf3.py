96529750bf91ea7c1e1c30e61ec70a45bf1d8944
weijiechua <weijiechua9512@gmail.com>
2022-03-03 21:47:21 +0000

    training mf

--- archived/training_mf3.py
+++ archived/training_mf3.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
   # model = SentimentClassifier.from_pretrained(PRE_TRAINED_MODEL_NAME).to(device)
 
   # Read the data file
-  df_train = pd.read_csv('datasets/df_downsample.csv', index_col=0)
+  df_train = pd.read_csv('datasets/training_data/data_augmentation/df_updown_paraphrased.csv', index_col=0)
   df_test = pd.read_csv('datasets/df_test.csv', index_col=0)
   trainset = reader(df_train.iloc[:5])
   testset = reader(df_test.iloc[:5])
